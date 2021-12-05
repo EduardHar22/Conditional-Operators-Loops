c5b0f2bfe38abedfb5408df975fd79fa78172b29
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:34:36 +0400

    feat: added lastToBegin.js file and did tasks

---
+++ lastToBegin.js
@@ -0,0 +1,15 @@
+// "use strict"
+
+// function lastToBegin(num){
+//     let percent = num % 10;
+//     let divide = parseInt(num / 10);
+//     let all = percent + "" + divide;
+//     if(num < 10){
+//         return num + '';
+//     }
+//     else if(percent === 0){
+//         return num + ''
+//     }
+//     return all;
+// }
+// console.log(lastToBegin(404))
