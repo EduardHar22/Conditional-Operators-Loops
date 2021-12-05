8e4ffb3916d84a53e6d66afc5fe23a698528c975
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:35:28 +0400

    feat: added isInOrNot.js file and did tasks

---
+++ isInOrNot.js
@@ -0,0 +1,13 @@
+// "use strict"
+
+// function inOrNot(num,isInNum){
+//     let str = num + '';
+//     let spl = str.split("")
+//     let str2 =  isInNum + ""
+//     if(spl.includes(str2)){
+//         return "yes"
+//     }
+//     else
+//         return "no"
+// }
+// console.log(inOrNot(3,3))
