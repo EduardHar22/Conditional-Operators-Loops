e5aa159364706231eef2d3722ee180c7601bff59
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:19:54 +0400

    feat: added whileDoWhile.js file and did tasks

---
+++ whileDowhile.js
@@ -0,0 +1,17 @@
+// "use strict"
+
+// let a = [1,2,3,4,5];
+// let i = 0;
+// while(i < a.length){
+//     console.log('while', i, a[i]);
+//     i++;
+// }
+
+
+// let a = [1,2,3,4,5];
+// let i = 0;
+// do{
+//     console.log('while', i, a[i]);
+//     i++;
+// }
+// while(i < a.length);
