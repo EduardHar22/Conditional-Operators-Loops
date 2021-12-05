f0cd75154dddb812e56951a2ad4003f701c2e3a6
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:33:51 +0400

    feat: added multiple.js file and did tasks

---
+++ multiple.js
@@ -0,0 +1,20 @@
+// "use strict"
+
+// let multiNum = [21,35,13,420,24];
+
+// for(let i = 0; i < multiNum.length; i++){
+//     if(multiNum[i] % 3 === 0 && multiNum[i] % 5 === 0 && multiNum[i] % 7 === 0){
+//         console.log(multiNum[i] + ' is a multiple of 3,5 and 7')
+//     }
+//     else if(multiNum[i] % 3 === 0 && multiNum[i] % 7 === 0){
+//         console.log(multiNum[i] + ' is a multiple of 3 and 7')
+//     }
+//     else if(multiNum[i] % 5 === 0 && multiNum[i] % 7 === 0){
+//         console.log(multiNum[i] + ' is a multiple of 5 and 7')
+//     }
+//     else if(multiNum[i] % 3 === 0){
+//         console.log(multiNum[i] + ' is a multiple of 3')
+//     }
+//     else
+//         console.log(multiNum[i] + ' is not a multiple of 3,5 and 7');
+// }
