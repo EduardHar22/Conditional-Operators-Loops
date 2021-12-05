926656804dbedefc2627309977bda1c8b5ce253a
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:29:12 +0400

    feat: added sort.js.js file and did tasks

---
+++ sort.js
@@ -0,0 +1,7 @@
+// "use strict"
+
+// let firstSorts = [45,26,78];
+// let secondSorts = [-23,-456,0];
+// console.log(firstSorts.sort((a, b) => a - b))
+// console.log(secondSorts.sort((a, b) => a - b))
+
