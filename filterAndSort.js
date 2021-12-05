f46f800be6cfcd77a67b12bc9a94132d1fa19a76
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:36:50 +0400

    feat: added filterAndSort.js file and did tasks

---
+++ filterAndSort.js
@@ -0,0 +1,15 @@
+// "use strict"
+
+// let filledArr2 = []
+// let filledArr = []
+// let filterArr1 = [8,0,1,"hey",12,5,true,"2",null,7,3];
+// for(let i = 0; i < filterArr1.length; i++){
+//     if(typeof filterArr1[i] === "number" && filterArr1[i] % 2 === 0){
+//         filledArr.push(filterArr1[i]);
+//     }
+//     if(filterArr1[i] % 2 !== 0 && typeof filterArr1[i] === "number"){
+//         filledArr2.push(filterArr1[i])
+//     }
+// }
+// let concatArr = filledArr2.concat(filledArr);
+// console.log(concatArr)
