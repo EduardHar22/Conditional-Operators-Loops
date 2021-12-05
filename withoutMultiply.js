b5e31d2fd5ccbdd6d81bce667396bb1d07233c46
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:26:47 +0400

    feat: added withoutMultiply.js file and did tasks

---
+++ withoutMultiply.js
@@ -0,0 +1,21 @@
+// "use strict"
+
+// function multi(num){
+//     let countMinus = 0;
+//     for(let i = 0; i < num.length; i++){
+//         if(num[i] === 0){
+//             return "unsigned"
+//         }
+//         if(num[i] < 0){
+//             countMinus++
+//         }
+       
+//     }
+//     if(countMinus % 2 === 0){
+//             return "+"
+//     }
+//     else
+//         return "-"
+
+// }
+// console.log(multi([]))
