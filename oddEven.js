92d6e0cbfe87ade18265d35b92f57e179794fac5
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:32:25 +0400

    feat: added oddEven.js file and did tasks

---
+++ oddEven.js
@@ -0,0 +1,10 @@
+// "use strict"
+
+// let oddEvenNumbers = [20,35,125];
+// for(let i = 0; i < oddEvenNumbers.length; i++){
+//     if(oddEvenNumbers[i] % 2 === 0){
+//         console.log(oddEvenNumbers[i] + ' -', 'even');
+//     }
+//     else
+//         console.log(oddEvenNumbers[i] + ' -', 'odd')  ;  
+// }
