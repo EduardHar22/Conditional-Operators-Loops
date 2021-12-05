1fe75bf6df5c0f8e979ee882b00027233c56220d
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:38:22 +0400

    feat: added countNumsStrs.js file and did tasks

---
+++ countNumsStrs.js
@@ -0,0 +1,16 @@
+// "use strict"
+
+// function countNumStr(arr){
+//     let countStr = 0;
+//     let countNum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(typeof arr[i] === "string"){
+//             countStr++;
+//         }
+//         else if(typeof arr[i] === "number"){
+//             countNum++;
+//         }
+//     }
+//     return "Number:" + countNum + ',' + "String:" + countStr;
+// }
+// console.log(countNumStr([2,"d"]))
