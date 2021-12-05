4a6166de4eb903b12245dee13d0d92772386f611
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:28:30 +0400

    feat: added splitSentence.js file and did tasks

---
+++ splitSentence.js
@@ -0,0 +1,9 @@
+// "use strict"
+
+// function splitSentence(sentence){
+//     let sliceEd = sentence.slice(0,-1);
+//     let splitEd = sliceEd.split(" ");
+//     return splitEd;
+// }
+// console.log(splitSentence("keep your friends close, but your enemies closer"))
+
