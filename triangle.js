f6611778d8b3838e589ed42802885d7a6dbbc79e
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-06 02:27:39 +0400

    feat: added triangle.js file and did tasks

---
+++ triangle.js
@@ -0,0 +1,25 @@
+// "use strict"
+
+// let firstAngles = [45,90];
+// let secondAngles = [30,30];
+// let thirdAngles = [75,25];
+// let sumOfTriangleAngles = 180;
+// let firstSum = 0;
+// let secondSum = 0;
+// let thirdSum = 0;
+// let clearSum1 = 0;
+// let clearSum2 = 0;
+// let clearSum3 = 0;
+
+// for(let i = 0; i < firstAngles.length, i < secondAngles.length, i < thirdAngles.length; i++){
+//     firstSum += firstAngles[i];
+//     secondSum += secondAngles[i];
+//     thirdSum += thirdAngles[i];
+//     clearSum1 = sumOfTriangleAngles - firstSum;
+//     clearSum2 = sumOfTriangleAngles - secondSum;
+//     clearSum3 = sumOfTriangleAngles - thirdSum;
+// };
+// console.log(clearSum1,clearSum3,clearSum2)
+
+
+
